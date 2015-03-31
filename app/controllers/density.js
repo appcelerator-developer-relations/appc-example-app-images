d8f2f95161b8afab50948dd39a6096cf04c85bc3
Fokke Zandbergen <mail@fokkezb.nl>
2015-03-31 11:24:17 +0200

    Improve red bar on iOS, remove old tab icons, add remote URL test, update screenshots

--- app/controllers/density.js
+++ app/controllers/density.js
@@ -27,9 +27,25 @@ var utils = require('utils');
 		});
 	}
 
-	// test if the device dpi exactly matches a category
-	var exact = [120,160,240,320,480,640].indexOf(Ti.Platform.displayCaps.dpi) !== -1;
+	if (!OS_IOS) {
 
-	$.widthText.text = $.widthText.text.replace('%s', exact ? 'EXACTLY' : 'NOT');
+		// device DPI does not exactly match a category
+		if ([120, 160, 240, 320, 480, 640].indexOf(Ti.Platform.displayCaps.dpi) !== -1) {
+			$.rulerLabel.text = $.rulerLabel.text.replace('%s', ' NOT');
+		}
+	}
+
+	// on iOS we can use remote density-specific images as well (note the hires attribute in the XML!)
+	if (OS_IOS) {
+
+		// but we have to set the URL to the right one ourselves
+		var ldf = Ti.Platform.displayCaps.logicalDensityFactor;
+		var url = 'http://dev.fokkezb.nl/devrel/';
+
+		// FIXME: remote @3x is not supported ATM
+		url += (ldf > 1) ? 'image%402x.png' : 'image.png';
+
+		$.remoteImage.image = url;
+	}
 
 })(arguments[0] || {});
